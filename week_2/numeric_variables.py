cb428fb1befbcd8e50b0d1eb8cfca20954ab70c6
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2023-11-01 20:48:54 -0600

    numeric variables practice

---
+++ week_2/numeric_variables.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you today? "))
+eggs= int(input("How many cartons of eggs do you have? "))
+cookies = int(input("How many cookies do you have? "))
+people = int(input("How many people are coming to your party? "))
+
+
+print(f"You will be {age + 1} on your next birthday. Do you feel old yet?")
+print(f"You have {eggs * 12} eggs. Did you buy enough?")
+print(f"You will have {cookies // people} whole cookies per person. Is that enough?")
