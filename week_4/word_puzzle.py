d8d531c8a0ef3bd60facedf333d517e6f4758222
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2023-11-24 08:23:44 -0700

    basic requirements

--- week_4/word_puzzle.py
+++ week_4/word_puzzle.py
@@ -5,6 +5,13 @@ correct = False
 
 tries = 0
 
+number_of_letters = len(word)
+
+for index_1 in range(number_of_letters):
+    letter = word[index_1]
+    print(f"{letter}, {index_1}")
+    
+
 #giving the hint
 print("Your hint is: ",)
 
@@ -12,18 +19,36 @@ for letter in word:
     print("_", end=" ")
 print("\n")
 guess = input("What is your guess: ").upper()
+number_of_letters_guess = len(guess)
 tries += 1
+if guess.upper() == word:
+        
+        correct = True
 #the puzzle
 while correct is False:
     
         #win or go again
-    if guess.upper() == word:
-        correct = True
+    if guess != word:
+        
+        for i in range(len(word)):
+            if i < len(guess):
+                if guess[i] == word[i]:
+                    # Same letter in the same position, print upper case
+                    print(guess[i].upper(), end=" ")
+                elif guess[i] in word:
+                    # Letter in both words but not in the same position, print lower case
+                    print(guess[i].lower(), end=" ")
+                else:
+                    # Letter not in the word, print underscore
+                    print("_", end=" ")
+        print("\n")
+        guess = input("Not quite, try again ").upper()
 
-    else:
-        guess = input("Not quite, try again: ")
         tries += 1
 
+    elif guess.upper() == word:
+        
+        correct = True
 if tries == 1:
     print("Great job, you got it right on the first guess!")
 else:
