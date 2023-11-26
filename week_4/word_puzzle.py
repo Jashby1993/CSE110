0e6c33ee56ed3d6374fbe35e0ceaf15fe03292a0
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2023-11-25 22:02:12 -0700

    Final Fix

--- week_4/word_puzzle.py
+++ week_4/word_puzzle.py
@@ -1,6 +1,23 @@
+#Extras: list of 100 words of different lengths, and the word is randomly picked every time. Option to play again. An option to give up.
+import random 
 
-word = "SARCASM"
 
+
+
+words = ["apple", "happy", "train", "cloud", "quick", "yellow", "orange", "python", "cookie", "sunset",
+              "garden", "jacket", "purple", "forest", "silver", "rocket", "puzzle", "flame", "wisdom", "planet",
+              "ocean", "river", "guitar", "camera", "rocket", "coffee", "museum", "hiking", "cloudy", "singer",
+              "butter", "summer", "travel", "breeze", "flower", "candle", "mirror", "keyboard", "diamond", "safari",
+              "monkey", "lemon", "sunset", "forest", "basket", "cheese", "cactus", "writer", "office", "soccer",
+              "blanket", "amazing", "journey", "whisper", "freedom", "captain", "mountain", "chocolate", "library",
+              "zebra", "dolphin", "rocket", "helmet", "sweater", "penguin", "fantasy", "swimmer", "sprinter",
+              "bicycle", "passion", "zodiac", "marble", "window", "silence", "moonlight", "alphabet", "shadow",
+              "courage", "umbrella", "laughter", "diamond", "compass", "treasure", "whisper", "thunder", "sunset",
+              "firefly", "journey", "harmony", "captain", "balance", "infinity", "whistle", "mystery", "heartbeat"]
+
+
+word = random.choice(words).lower()
+guess = " "
 correct = False
 
 tries = 0
@@ -9,7 +26,7 @@ number_of_letters = len(word)
 
 for index_1 in range(number_of_letters):
     letter = word[index_1]
-    print(f"{letter}, {index_1}")
+    #print(f"{letter}, {index_1}")
     
 
 #giving the hint
@@ -18,42 +35,59 @@ print("Your hint is: ",)
 for letter in word:
     print("_", end=" ")
 print("\n")
-guess = input("What is your guess: ").upper()
-number_of_letters_guess = len(guess)
-tries += 1
-if guess.upper() == word:
-        
-        correct = True
+
 #the puzzle
 while correct is False:
-    
-        #win or go again
-    if guess != word:
-        
-        for i in range(len(word)):
-            if i < len(guess):
-                if guess[i] == word[i]:
-                    # Same letter in the same position, print upper case
-                    print(guess[i].upper(), end=" ")
-                elif guess[i] in word:
-                    # Letter in both words but not in the same position, print lower case
-                    print(guess[i].lower(), end=" ")
-                else:
-                    # Letter not in the word, print underscore
-                    print("_", end=" ")
-        print("\n")
-        guess = input("Not quite, try again ").upper()
-
-        tries += 1
-
-    elif guess.upper() == word:
-        
+    guess = input("What is your guess: ").lower()
+    number_of_letters_guess = len(guess)
+    tries += 1
+    if guess == word:
         correct = True
-if tries == 1:
-    print("Great job, you got it right on the first guess!")
-else:
-    print(f"You got it! You found the word in {tries} tries.")
         
+    else:
+        if guess == "i give up.":
+            print(f"That's ok, let's try a different word! The word was {word}.")
+            correct = True
+        elif number_of_letters == number_of_letters_guess:
+        
+            for i in range(len(word)):
+                if i < len(guess):
+                    if guess[i] == word[i]:
+                        # Same letter in the same position, print upper case
+                        print(guess[i].upper(), end=" ")
+                    elif guess[i] in word:
+                        # Letter in both words but not in the same position, print lower case
+                        print(guess[i].lower(), end=" ")
+                    else:
+                        # Letter not in the word, print underscore
+                        print("_", end=" ")
+            print("\n")
+            #guess = input("Try again ").upper()
+        elif number_of_letters_guess > number_of_letters:
+            print("Too many letters, try a smaller word!")
+           
+           
+            
+            
+        elif number_of_letters_guess < number_of_letters:
+            print("Not enough letters, try a longer word!")
+        
+        
+            
+
+
+    
+if guess == "i give up":
+    print(f"You gave it {tries}. Better luck next time!")
+else: 
+    if tries == 1:
+        print("Great job, you got it right on the first guess!")
+    else:
+        print(f"You got it! You found the word in {tries} tries.")
+
+
+
+
 
 
 
